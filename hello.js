9741dfa21a33818914fb4339fd0132124cdf25fa
gopalatak6992 <gopalatak6992@gmail.com>
2023-05-05 21:51:13 +0530

    added a personal information
    added a personal information

---
+++ hello.js
@@ -0,0 +1,15 @@
+console.log("Gopal Atak..");
+var firstName = "Gopal";
+console.log(firstName)
+
+var age; // variable Declaration
+age = "23"; // variable initialization
+console.log(age);
+
+var holder;
+console.log(holder);
+holder = "My spec";//variable Initilization
+console.log(holder);
+
+holder = "sachin spec";//variable update
+console.log(holder);
---
+++ variable_assign.js
@@ -0,0 +1,17 @@
+console.log("Gopal Atak");
+var fullName="Gopal Baburao Atak";
+console.log(fullName);
+
+var mylove;
+mylove = "I love only JS";
+console.log(mylove);
+
+var firstname,lastname,age,address,pincode,gender,Ismarried;
+firstname = "Gopal";
+lastname = "Atak";
+age = "22";
+address = "At Gajarkhed Tq mehkar Dist Buldhana";
+pincode = "443301";
+gender = "male";
+Ismarried = "No";
+console .log(firstname,lastname,age,address,pincode,gender,Ismarried);

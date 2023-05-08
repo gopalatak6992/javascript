92e9d6861aa4ca3ff15f71d52589bc1373b29c2f
gopalatak6992 <gopalatak6992@gmail.com>
2023-05-08 20:42:43 +0530

    Create a Variable Basic assignment
    Create a Variable Basic assignment

---
+++ variableBasicAssig.js
@@ -0,0 +1,55 @@
+
+
+var math = 80;
+var science = 85;
+var physics = 90;
+console.log(math, science, physics);
+var typeOfMath = typeof math;
+console.log( "Type of variable -> Math:80,  type:",typeof math );
+
+var typeofScience = typeof science;
+console.log("Type of Variable -> Science:85, type:",typeof science);
+
+var typeofPhysics = typeof physics;
+console.log("Type of Variable -> Physics:90, type:",typeof physics);
+
+
+
+var indianCaptain = "Rohit Sharma";
+var cskCaptain =" Mahindra Singh Dhoni";
+var rcbCaptain = "Virat Kohli";
+console.log(indianCaptain, cskCaptain,  rcbCaptain);
+
+
+
+var typeofIndianCaptain = typeof indianCaptain ;
+console.log("Type of Variable->IndianCaptain:Rohit Sharma, type:",typeof indianCaptain);
+
+var typeofCskCaptain= typeof cskCaptain ;
+console.log("Type of Variable->CskCaptain:Mahindra Singh Dhoni, type:",typeof cskCaptain);
+
+var typeofRcbCaptain = typeof rcbCaptain;
+console.log("Type of Variable->RcbCaptain:Virat Kohli, type:", typeof rcbCaptain);
+
+
+var isMarried = false;
+var isDeveloper = true;
+var isPostGraduate = true;
+console.log(isMarried. isDeveloper, isPostGraduate);
+
+var typeofIsMarried = typeof isMarried;
+console.log("Type of Variable->IsMarried:false, type:", typeof isMarried);
+
+var typeofIsDeveloper = typeof isDeveloper;
+console.log("Type of Variable->IsDeveloper:true, type:", typeof isDeveloper);
+
+var typeofIsPostGraduate = typeof isPostGraduate;
+console.log("Type of Variable->IsPostGraduate:true, type:", typeof isPostGraduate);
+
+var aadharcard;
+console.log("Aadharcard: ", aadharcard);
+console.log("type of variable Aadharcard is", typeof aadharcard);
+
+var bankDetails = null;
+console.log("BankDetails:",bankDetails);
+console.log("Type of variable BankDetails is", typeof bankDetails);

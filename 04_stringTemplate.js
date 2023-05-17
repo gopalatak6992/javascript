de1b795b0c602cd6e80f498072a504dc86df81a6
gopalatak6992 <gopalatak6992@gmail.com>
2023-05-17 22:14:10 +0530

    operator assignment
    operator assignment

---
+++ 04_stringTemplate.js
@@ -0,0 +1,49 @@
+
+
+
+var greet = "Good Morning mate, How are you";
+var resultSplit = greet.split(" ");
+console.log(resultSplit);
+//console.log("After slipt words are:",resultSplit, "Total number of words: ",resultSplit.length);
+console.log(`After slipt words are ${resultSplit} Total number of words ${resultSplit.length} `);
+
+console.log(`Given string is: ${greet}`);
+
+var myName = "Elon Musk";
+var result = myName.startsWith("E");
+console.log(` Is ${myName} starts with character E: ${result}`);
+
+var result = myName.startsWith("Elon");
+console.log(` Is ${myName} starts with character "Elon" : ${result}`);
+
+var result = myName.startsWith("elon");
+console.log(` Is ${myName} starts with character "elon" : ${result}`);
+
+
+var myName = "Elon Musk";
+var result = myName.endsWith("k");
+console.log(` Is ${myName} ends with character "k" : ${result}`);
+
+var myName = "Elon Musk";
+var result = myName.endsWith("Musk");
+console.log(` Is ${myName} ends with character "Musk" : ${result}`);
+
+var myName = "Elon Musk";
+var result = myName.endsWith("b");
+console.log(` Is ${myName} ends with character "b" : ${result}`);
+
+
+ar multiplication = function (one, two, three) {
+    var result = one*two*three;
+    return `Multiplication is: ${result}`;
+}
+var result = multiplication(2, 2, 2 );
+console.log(result);
+
+Codemind Technology 8:34 AM
+var multiplication = function (one, two, three) {
+    var result = one*two*three;
+    return `Multiplication is: ${result}`;
+}
+var result = multiplication(2, 2, 2 );
+console.log(result);
